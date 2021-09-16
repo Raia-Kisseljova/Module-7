8170d12dc4267faac2ec58e4b691b57e924cea24
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-09-16 15:54:30 +0100

    styling added

--- day-1-search-engine/src/Components/CompanyDetail.jsx
+++ day-1-search-engine/src/Components/CompanyDetail.jsx
@@ -1,54 +1,65 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Container, Table, Row, Col } from "react-bootstrap";
 
 export default function CompanyDetail(props) {
   const [company, setCompany] = React.useState([]);
   console.log(props);
   React.useEffect(() => {
     const response = fetch(
-      `https://strive-jobs-api.herokuapp.com/jobs?company=${companyName}`
+      `https://strive-jobs-api.herokuapp.com/jobs?company=${companyNameURL}`
     )
       .then((response) => response.json())
       .then((data) => setCompany(data.data));
   }, []);
 
-  const companyName = window.location.pathname.split("/")[2];
+  const companyNameURL = window.location.pathname.split("/")[2];
+
   return (
     <>
-      <div className="mb-5">{company[0].company_name}</div>
-      {company.map((c) => {
-        return (
-          <>
-            <div>{c.company_name}</div>
-            <div className="company-description">
-              {c.description.replace(/<[^>]+>/g, "")}
-            </div>
-            <div>Found jobs : {company.length}</div>
-            <Table striped bordered hover variant="dark">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Salary</th>
-                  <th>Category</th>
-                  <th>Find out more</th>
-                </tr>
-              </thead>
-              <tbody>
-                {company.map((c) => {
-                  return (
-                    <tr>
-                      <td>{c.title}</td>
-                      <td>{c.salary}</td>
-                      <td>{c.category}</td>
-                      <td>{c.url}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </>
-        );
-      })}
+      <Container>
+        <Row>
+          <Col md={10}>
+            {company.map((c) => {
+              return (
+                <>
+                  <div className="my-3 text-center">{c.company_name}</div>
+                  <div className="company-description">
+                    <p className="text-center">About company</p>
+                    {c.description.replace(/<[^>]+>/g, "")}
+                  </div>
+                  <div className="my-3 text-center">
+                    Found jobs : {company.length}
+                  </div>
+                  <Table striped bordered hover variant="dark">
+                    <thead>
+                      <tr>
+                        <th>Title</th>
+                        <th>Salary</th>
+                        <th>Category</th>
+                        <th>Apply here</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {company.map((c) => {
+                        return (
+                          <tr>
+                            <td>{c.title}</td>
+                            <td>{c.salary}</td>
+                            <td>{c.category}</td>
+                            <td>
+                              <a href={c.url}>{c.url}</a>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </Table>
+                </>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
